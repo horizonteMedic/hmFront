99bd51c84d1dfa5ba29c7e39aa6b733a453513fe
horizonteMedic <sistemas@horizontemedic.com>
2024-05-03 00:03:17 -0500

    cambios de reporte

--- src/views/admin/panel-de-control/Reportes/Reporte.jsx
+++ src/views/admin/panel-de-control/Reportes/Reporte.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlus, faSearch, faSyncAlt } from '@fortawesome/free-solid-svg-icons';
+import { faPlus, faSearch, faSyncAlt, faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 import { ComboboxSedes } from './Modal/Combobox';
 import { GetListREport } from './model/getlistreport';
 import { useAuthStore } from '../../../../store/auth';
@@ -138,7 +138,14 @@ const HistorialPaciente = () => {
         setIsReloading(false); // Establece isReloading en false cuando la recarga ha finalizado
       });
   };
-  
+  const visiblePages = () => {
+    const totalVisiblePages = 5; // Número de páginas visibles
+    const halfVisiblePages = Math.floor(totalVisiblePages / 2);
+    let startPage = currentPage - halfVisiblePages;
+    startPage = Math.max(startPage, 1); // No puede ser menor que 1
+    const endPage = startPage + totalVisiblePages - 1;
+    return Array.from({ length: totalVisiblePages }, (_, i) => startPage + i).filter(page => page <= totalPages);
+  };
 
   const startIdx = (currentPage - 1) * recordsPerPage;
   const endIdx = startIdx + recordsPerPage;
@@ -147,52 +154,55 @@ const HistorialPaciente = () => {
   return (
     <div className="container mx-auto mt-12 mb-12">
       <div className="mx-auto bg-white rounded-lg overflow-hidden shadow-xl w-[90%]">
-        <div className="px-4 py-2 azuloscurobackground flex justify-between items-center"> {/* Agrega items-center para centrar verticalmente el icono */}
+        <div className="px-4 py-2 azuloscurobackground flex justify-between items-center"> 
           <h1 className="text-start font-bold color-azul text-white">Reporte de Pacientes</h1>
-          <button onClick={reloadTable} className="focus:outline-none ml-3 relative"> {/* Agrega el botón de recarga */}
+          <button onClick={reloadTable} className="focus:outline-none ml-3 relative">
             {loading && <div className="absolute inset-0 opacity-50 rounded-md"></div>}
             <FontAwesomeIcon icon={faSyncAlt} className={`text-white cursor-pointer tamañouno ${loading ? 'opacity-50' : ''}`} />
           </button>
 
         </div>
-        <div className="flex justify-between items-center ml-4 pt-3">
-          <div className="flex items-center">
-            <span>Mostrar</span>
-            <select className="border pointer border-gray-300 rounded-md ml-2 px-2 py-1" value={recordsPerPage} onChange={handleChangeRecordsPerPage}>
-              <option value={5}>5</option>
-              <option value={10}>10</option>
-              <option value={15}>15</option>
-              <option value={20}>20</option>
-            </select>
-            <span className='ml-2'> registros </span>
-            <span className="ml-6"><strong>Fecha inicio: </strong></span>
-            <input
-              type="date"
-              className="pointer border border-gray-300 rounded-md ml-2 px-2 py-1 w-36"
-              value={startDate}
-              onChange={handleStartDateChange}
-            />
-            <span className="ml-6"><strong>Fecha fin:</strong></span>
-            <input
-              type="date"
-              className=" pointer border border-gray-300 rounded-md ml-2 px-2 py-1 w-36"
-              value={endDate}
-              onChange={handleEndDateChange}
-            />
-            <span className="ml-6 mr-3"><strong> Sedes:  </strong> </span>
-            <select
-              className="pointer border border-gray-300 px-3 py-2 rounded-md w-90 focus:outline-none bg-white"
-              onChange={(e) => setSede(e.target.value)}
-              required
-              value={sede}
-            >
-              <option value=""> Seleccionar</option>
-              {ListSedes?.map((option) => (
-                <option key={option.cod_sede} value={option.cod_sede}>{option.nombre_sede}</option>
-              ))}
-            </select>
-          </div>
-        </div>
+        <div className="flex flex-col justify-between items-center md:flex-row mr-12 ml-12 pt-3">
+      <div className="flex flex-col justify-between items-center md:flex-row w-full md:w-auto mb-4 md:mb-0 md:mr-4"> 
+        <span className="mr-2 md:mr-4">Mostrar</span>
+        <select className="border pointer border-gray-300 rounded-md px-2 py-1 mb-2 md:mb-0 md:mr-4" value={recordsPerPage} onChange={handleChangeRecordsPerPage}>
+          <option value={5}>5</option>
+          <option value={10}>10</option>
+          <option value={15}>15</option>
+          <option value={20}>20</option>
+        </select>
+        <span className='mr-2 md:mr-4'>registros</span>
+      </div>
+      <div className="flex flex-col justify-between items-center md:flex-row w-full md:w-auto focus:outline-none"> 
+        <span className="mr-2 md:mr-4"><strong>Fecha inicio:</strong></span>
+        <input
+          type="date"
+          className="pointer border border-gray-300 rounded-md px-2 py-1 mb-2 w-full md:w-auto focus:outline-none md:mr-4"
+          value={startDate}
+          onChange={handleStartDateChange}
+        />
+        <span className="mr-2 md:mr-4"><strong>Fecha fin:</strong></span>
+        <input
+          type="date"
+          className="pointer border border-gray-300 rounded-md px-2 py-1 mb-2 md:mb-0 w-full md:w-auto focus:outline-none md:mr-4"
+          value={endDate}
+          onChange={handleEndDateChange}
+        />
+        <span className="mr-2 md:mr-4"><strong>Sedes:</strong></span>
+        <select
+          className="pointer border border-gray-300 px-3 py-2 rounded-md w-full md:w-auto focus:outline-none" 
+          onChange={(e) => setSede(e.target.value)}
+          required
+          value={sede}
+        >
+          <option value="">Seleccionar</option>
+          {ListSedes?.map((option) => (
+            <option key={option.cod_sede} value={option.cod_sede}>{option.nombre_sede}</option>
+          ))}
+        </select>
+      </div>
+    </div>
+
         <div className="overflow-x-auto p-3">
   {loading ? (
     <p className="text-center">Cargando...</p>
@@ -228,8 +238,18 @@ const HistorialPaciente = () => {
 
         {/* Renderiza los botones de paginación */}
         <div className="flex justify-center p-4">
-          <button onClick={() => setCurrentPage(currentPage - 1)} disabled={currentPage === 1} className="mx-1 px-3 py-1 bg-blue-500 text-white rounded-md">Anterior</button>
-          <button onClick={() => setCurrentPage(currentPage + 1)} disabled={currentPage === totalPages} className="mx-1 px-3 py-1 bg-blue-500 text-white rounded-md">Siguiente</button>
+          <button onClick={() => setCurrentPage(currentPage - 1)} disabled={currentPage === 1} className="mx-1 px-3 py-1 bg-blue-500 text-white rounded-md">
+            <FontAwesomeIcon icon={faChevronLeft} />
+          </button>
+          {/* Mostrar números de página */}
+          {visiblePages().map((page) => (
+            <button key={page} onClick={() => handlePageClick(page)} className={`mx-1 px-3 py-1 rounded-md ${currentPage === page ? 'bg-blue-500 text-white' : 'bg-gray-200'}`}>
+              {page}
+            </button>
+          ))}
+          <button onClick={() => setCurrentPage(currentPage + 1)} disabled={currentPage === totalPages} className="mx-1 px-3 py-1 bg-blue-500 text-white rounded-md">
+            <FontAwesomeIcon icon={faChevronRight} />
+          </button>
         </div>
       </div>
       {isModalOpen && <Modal closeModal={closeModal} user={userlogued.sub} start={startDate} end={endDate} sede={sede} dni={dnipicker} nombre={nombrespicker} token={token} />}
