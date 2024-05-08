07d28e22b7385065df7ccc092b12a0f7fb6f4b26
repoHM <sistemas@horizontemedic.com>
2024-05-07 22:51:13 -0500

    roles delete

--- src/views/admin/panel-de-control/Roles/model/DeleteRol.js
+++ src/views/admin/panel-de-control/Roles/model/DeleteRol.js
@@ -1,31 +1,17 @@
 import { URLAzure } from "../../../../config/config";
 
 //El rol no se borra, solo se pone su estado en false
-export default async function DeleteRol(id,rol,descripcion,fecharegistro,userRegistro,token,userlogued){
+export default async function DeleteRol(id,token){
 
-    const currentDate = new Date(); // Obtiene la fecha y hora actual
-    const year = currentDate.getFullYear(); // Obtiene el año actual
-    const month = ('0' + (currentDate.getMonth() + 1)).slice(-2); // Obtiene el mes actual y le agrega un 0 al principio si es menor a 10
-    const day = ('0' + currentDate.getDate()).slice(-2); 
-
-    const data = {
-        nombre: rol,
-        estado: false,
-        fechaRegistro: fecharegistro,
-        userRegistro: userRegistro,
-        fechaActualizacion: `${year}-${month}-${day}`,
-        userActualizacion: userlogued,
-        descripcion: descripcion
-    }
     const response = await fetch(`${URLAzure}/api/v01/ct/rol/${id}`, {
-            method: 'PUT', 
+            method: 'DELETE', 
             headers: {
                 'Content-Type': 'application/json',
                 'Authorization': `Bearer ${token}`
-            },
-            body: JSON.stringify(data)
+            }
 
         })
+
         if(response.ok){
             return
         } else {
