f1b538bab27a24b9c4c9fd2dffb445f3b9728eab
horizonteMedic <sistemas@horizontemedic.com>
2024-06-03 09:15:10 -0500

    reci

--- src/views/admin/panel-de-control/Reportes/DataUploadModal/DataUploadModal.jsx
+++ src/views/admin/panel-de-control/Reportes/DataUploadModal/DataUploadModal.jsx
@@ -3,19 +3,19 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTimes, faFolder, faCheck, faTimesCircle } from '@fortawesome/free-solid-svg-icons'; 
 import Swal from 'sweetalert2';
 import ArchivosMasivos from '../model/postArchivosMasivos';
-import {Loading} from '../../../../components/Loading'
+import { Loading } from '../../../../components/Loading';
 
 const DataUploadModal = ({ closeModal, Sedes, user, token }) => {
-  const [uparchFile, setUparchFile] = useState([])
+  const [uparchFile, setUparchFile] = useState([]);
   const [uploadedFiles, setUploadedFiles] = useState([]);
   const [selectedSede, setSelectedSede] = useState('');
   const [isFolderUploadEnabled, setIsFolderUploadEnabled] = useState(false);
   const [isUploading, setIsUploading] = useState(false);
   const [uploadStatus, setUploadStatus] = useState({});
-  const [sucred, setSucred] = useState(false)
+  const [sucred, setSucred] = useState(false);
 
-  const sedes = Sedes
-  
+  const sedes = Sedes;
+
   const isImageOrPDFOrExcel = (fileName) => {
     const ext = fileName.split('.').pop().toLowerCase();
     return ext === 'jpg' || ext === 'jpeg' || ext === 'png' || ext === 'gif' || ext === 'pdf' || ext === 'xls' || ext === 'xlsx';
@@ -26,7 +26,7 @@ const DataUploadModal = ({ closeModal, Sedes, user, token }) => {
     setUploadStatus({});
 
     const folders = Array.from(e.target.files);
-    setUparchFile(folders)
+    setUparchFile(folders);
     const folderNames = folders.map((folder) => folder.name);
     setUploadedFiles((prevFiles) => [...prevFiles, ...folderNames]);
 
@@ -46,44 +46,44 @@ const DataUploadModal = ({ closeModal, Sedes, user, token }) => {
       }
     });
   };
- 
+
   const SubidaArchivos = async () => {
     let failedUploads = [];
-   
-      const uploadPromises = uparchFile.map(async (folder) => {
-        try {
-          const fileBase64 = await toBase64(folder);
-          const base64WithoutHeader = fileBase64.split(',')[1];
-          const datos = {
-            nombre: folder.name,
-            sede: selectedSede,
-            base64: base64WithoutHeader
-          };
-          const response = await ArchivosMasivos(datos, user, token);
-          if (response.id === 0) {
-            setUploadStatus((prevStatus) => ({
-              ...prevStatus,
-              [folder.name]: 'error',
-            }));
-            failedUploads.push(folder.name);
-          }
-        } catch (error) {
-          console.error(`Error uploading ${folder.name}:`, error);
+
+    const uploadPromises = uparchFile.map(async (folder) => {
+      try {
+        const fileBase64 = await toBase64(folder);
+        const base64WithoutHeader = fileBase64.split(',')[1];
+        const datos = {
+          nombre: folder.name,
+          sede: selectedSede,
+          base64: base64WithoutHeader
+        };
+        const response = await ArchivosMasivos(datos, user, token);
+        if (response.id === 0) {
+          setUploadStatus((prevStatus) => ({
+            ...prevStatus,
+            [folder.name]: 'error',
+          }));
           failedUploads.push(folder.name);
         }
-      });
-
-      await Promise.all(uploadPromises);
-      setSucred(true)
-      setIsUploading(false);
-      if (failedUploads.length > 0) {
-        Swal.fire({
-          icon: 'error',
-          title: 'Detención!',
-          text: `Estos Archivos no se han subido: ${failedUploads.join(', ')}`,
-        });
+      } catch (error) {
+        console.error(`Error uploading ${folder.name}:`, error);
+        failedUploads.push(folder.name);
       }
-  }
+    });
+
+    await Promise.all(uploadPromises);
+    setSucred(true);
+    setIsUploading(false);
+    if (failedUploads.length > 0) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Detención!',
+        text: `Estos Archivos no se han subido: ${failedUploads.join(', ')}`,
+      });
+    }
+  };
 
   const handleConfirmUpload = async () => {
     if (!selectedSede || uploadedFiles.length === 0) {
@@ -101,7 +101,7 @@ const DataUploadModal = ({ closeModal, Sedes, user, token }) => {
     }).then((result) => {
       if (result.isConfirmed) {
         setIsUploading(true);
-        SubidaArchivos()
+        SubidaArchivos();
       }
     });
   };
@@ -115,6 +115,14 @@ const DataUploadModal = ({ closeModal, Sedes, user, token }) => {
     });
   };
 
+  const getSuccessfulUploadsCount = () => {
+    return Object.values(uploadStatus).filter(status => status === 'success').length;
+  };
+
+  const getFailedUploadsCount = () => {
+    return Object.values(uploadStatus).filter(status => status === 'error').length;
+  };
+
   return (
     <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-gray-900 bg-opacity-50">
       <div className="mx-auto bg-white rounded-lg overflow-hidden shadow-md w-[600px] relative">
@@ -126,9 +134,9 @@ const DataUploadModal = ({ closeModal, Sedes, user, token }) => {
           <div className="bg-white rounded-lg z-50">
             <div className="flex mb-4">
               <select id="sedes" className="pointer border rounded-md px-2 py-1" value={selectedSede} onChange={(e) => {
-                  setSelectedSede(e.target.value);
-                  setIsFolderUploadEnabled(true);
-                }}>
+                setSelectedSede(e.target.value);
+                setIsFolderUploadEnabled(true);
+              }}>
                 <option value="">Seleccionar Sede</option>
                 {sedes?.map((option) => (
                   <option key={option.cod_sede} value={option.cod_sede}>{option.nombre_sede}</option>
@@ -145,7 +153,12 @@ const DataUploadModal = ({ closeModal, Sedes, user, token }) => {
 
             {uploadedFiles.length > 0 && (
               <div className="max-h-[300px] overflow-y-auto">
-                <h3 className="text-lg font-bold mb-2">Archivos Cargados:</h3>
+                <h3 className="text-lg font-bold mb-2">
+                  Archivos Cargados: {uploadedFiles.length}
+                </h3>
+                <h3 className="text-lg font-bold mb-2">
+                  Archivos con Error: {getFailedUploadsCount()}
+                </h3>
                 <table className="table-auto w-full">
                   <thead>
                     <tr>
@@ -164,8 +177,8 @@ const DataUploadModal = ({ closeModal, Sedes, user, token }) => {
                         {sucred && <td className="border px-4 py-2">
                           {uploadStatus[file] === 'success' ? (
                             <>
-                            <FontAwesomeIcon icon={faCheck} className="text-green-500" />
-                            <p>Archivo Subido Correctamente</p>
+                              <FontAwesomeIcon icon={faCheck} className="text-green-500" />
+                              <p>Archivo Subido Correctamente</p>
                             </>
                           ) : (
                             <FontAwesomeIcon icon={faTimesCircle} className="text-red-500" />
@@ -185,17 +198,16 @@ const DataUploadModal = ({ closeModal, Sedes, user, token }) => {
                 )}
               </div>
 
-            <button
-              className={`px-4 py-2 rounded mr-2 ${Object.values(uploadStatus).some(status => status === 'error') ? 'bg-gray-300 cursor-not-allowed' : 'bg-blue-500 text-white'}`}
-              onClick={handleConfirmUpload}
-              disabled={Object.values(uploadStatus).some(status => status === 'error')}
-            >
-              Subir
-            </button>
-
-              <button onClick={closeModal} className="bg-gray-300 px-4 py-2 rounded">Cerrar</button>
+              <button
+                className={`px-4 py-2 rounded mr-2 ${Object.values(uploadStatus).some(status => status === 'error') ? 'bg-gray-400 cursor-not-allowed' : 'bg-blue-500 text-white'
+                  }`}
+                disabled={Object.values(uploadStatus).some(status => status === 'error')}
+                onClick={handleConfirmUpload}
+              >
+                Subir Archivos
+              </button>
+              {isUploading && <Loading />}
             </div>
-            {isUploading && <Loading/>}
           </div>
         </div>
       </div>
