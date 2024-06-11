321563f8357a07cc345e8944ca7a23987359b8ab
horizonteMedic <sistemas@horizontemedic.com>
2024-06-11 12:56:32 -0500

    subir

--- src/views/admin/panel-de-control/Configuracion/Protocolos/Protocolos.jsx
+++ src/views/admin/panel-de-control/Configuracion/Protocolos/Protocolos.jsx
@@ -1,11 +1,7 @@
 import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-<<<<<<< HEAD
-import { faEdit, faSearch, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
-=======
 import Swal from 'sweetalert2';
 import { faEdit, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
->>>>>>> 0d90639f776ffc7c7e18d9bc3370e30213eb5697
 import RuterConfig from '../RuterConfig';
 import EditModal from '../Protocolos/EditModal/EditModal.jsx';
 import ModalRegistroServicios from './ModalRegistroServicios/ModalRegistroServicios.jsx'; // Importamos el nuevo componente
@@ -53,20 +49,6 @@ const Protocolos = () => {
     setServicios([...servicios, { id: option.idServicio, Nombre: option.nombreServicio, Precio: option.precio }]);
   };
 
-<<<<<<< HEAD
-  const handleContrataChange = (event) => {
-    const selectedContrata = event.target.value;
-    const newTableData = [...tableDataContratas, { orden: tableDataContratas.length + 1, contrata: selectedContrata }];
-    setTableDataContratas(newTableData);
-  };
-
-  const handleServicioChange = (event) => {
-    const selectedServicio = event.target.value;
-    const newTableData = [...tableDataServicios, { orden: tableDataServicios.length + 1, servicio: selectedServicio }];
-    setTableDataServicios(newTableData);
-  };
-
-=======
   const handleChange = (e) => {
     const selectedOption = JSON.parse(e.target.value);
     setSelectValue('')
@@ -162,7 +144,6 @@ const Protocolos = () => {
       });
   }
 
->>>>>>> 0d90639f776ffc7c7e18d9bc3370e30213eb5697
   return (
     <div className="container mx-auto mt-12 mb-12">
       <RuterConfig /> 
@@ -176,21 +157,6 @@ const Protocolos = () => {
             <label htmlFor="nombre" className="block font-medium text-gray-700 mr-2">
               Nombre:
             </label>
-<<<<<<< HEAD
-            <div className="flex items-center w-full">
-              <input
-                id="nombre"
-                name="nombre"
-                type="text"
-                className="border mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
-                placeholder="Ingrese el nombre del Protocolo"
-              />
-              <button className="border ml-2 flex items-center px-2 py-1 text-sm text-gray-500 border-gray-300 rounded-md">
-                Buscar
-                <FontAwesomeIcon icon={faSearch} className="ml-1" />
-              </button>
-            </div>
-=======
             <input
               id="nombre"
               name="nombre"
@@ -201,7 +167,6 @@ const Protocolos = () => {
               placeholder="Ingrese el nombre del Protocolo"
             />
             <FontAwesomeIcon icon={faEdit} className="text-blue-500 ml-2 cursor-pointer"  />
->>>>>>> 0d90639f776ffc7c7e18d9bc3370e30213eb5697
           </div>
 
           <div className="mb-4">
@@ -243,14 +208,9 @@ const Protocolos = () => {
           </div>
 
           <div className="mb-4 mt-6 flex flex-wrap">
-<<<<<<< HEAD
-            <div className="w-full md:w-1/2 pr-2">
-              <p className="mb-2">Listado de Contratas:</p>
-=======
 
             <div className="w-full md:w-1/2 pr-2">
               <p className="mb-2">Listado de Servicios:</p>
->>>>>>> 0d90639f776ffc7c7e18d9bc3370e30213eb5697
               <div className="mb-4">
                 <select
                   id="servicios"
@@ -258,40 +218,7 @@ const Protocolos = () => {
                   value={selectValue}
                   onChange={handleChange}
                   className="border mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
-                  onChange={handleContrataChange}
                 >
-<<<<<<< HEAD
-                                  <option disabled selected value="">Seleccionar</option>
-
-                  <option value="1">Opción 1</option>
-                  <option value="2">Opción 2</option>
-                  <option value="3">Opción 3</option>
-                </select>
-              </div>
-              <div className="mb-4 overflow-auto max-h-60">
-                <table className="w-full border-collapse border border-gray-300">
-                  <thead className="bg-gray-100">
-                    <tr>
-                      <th className="border border-gray-300 px-4 py-2">Orden</th>
-                      <th className="border border-gray-300 px-4 py-2">Contrata</th>
-                      <th className="border border-gray-300 px-4 py-2">Precio</th>
-                      <th className="border border-gray-300 px-4 py-2">Acciones</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {tableDataContratas.map((item) => (
-                      <tr key={item.orden} className="bg-white">
-                        <td className="border border-gray-300 px-4 py-2">{item.orden}</td>
-                        <td className="border border-gray-300 px-4 py-2">{item.contrata}</td>
-                        <td className="border border-gray-300 px-4 py-2">$100</td>
-                        <td className="border border-gray-300 px-4 py-2">
-                          <button className="text-blue-500">
-                            <FontAwesomeIcon icon={faEdit} />
-                          </button>
-                        </td>
-                      </tr>
-                    ))}
-=======
                   <option value="">Elija sus Servicios</option>
                   {Servicios?.map((option, index) => (
                     <option key={index} value={JSON.stringify(option)}>{option.nombreServicio}</option>
@@ -327,7 +254,6 @@ const Protocolos = () => {
                       </tr>
                     ))}
                     
->>>>>>> 0d90639f776ffc7c7e18d9bc3370e30213eb5697
                   </tbody>
                 </table>
               </div>
@@ -339,44 +265,6 @@ const Protocolos = () => {
             <div className="w-full md:w-1/2 pl-2">
               <p className="mb-2">Listado de Contratas:</p>
               <div className="mb-4">
-<<<<<<< HEAD
-                <select
-                  id="servicio"
-                  name="servicio"
-                  className="border mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline
-                  focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
-                  onChange={handleServicioChange}
-                >                <option disabled selected value="">Seleccionar</option>
-
-                  <option value="1">Opción 1</option>
-                  <option value="2">Opción 2</option>
-                  <option value="3">Opción 3</option>
-                </select>
-              </div>
-              <div className="mb-4 overflow-auto max-h-60">
-                <table className="w-full border-collapse border border-gray-300">
-                  <thead className="bg-gray-100">
-                    <tr>
-                      <th className="border border-gray-300 px-4 py-2">Orden</th>
-                      <th className="border border-gray-300 px-4 py-2">Servicio</th>
-                      <th className="border border-gray-300 px-4 py-2">Precio</th>
-                      <th className="border border-gray-300 px-4 py-2">Acciones</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {tableDataServicios.map((item) => (
-                      <tr key={item.orden} className="bg-white">
-                        <td className="border border-gray-300 px-4 py-2">{item.orden}</td>
-                        <td className="border border-gray-300 px-4 py-2">{item.servicio}</td>
-                        <td className="border border-gray-300 px-4 py-2">$100</td>
-                        <td className="border border-gray-300 px-4 py-2">
-                          <button className="text-blue-500">
-                            <FontAwesomeIcon icon={faEdit} />
-                          </button>
-                        </td>
-                      </tr>
-                    ))}
-=======
 
                 <input
                   id="contrata"
@@ -434,7 +322,6 @@ const Protocolos = () => {
                       </tr>
                     ))}
                     
->>>>>>> 0d90639f776ffc7c7e18d9bc3370e30213eb5697
                   </tbody>
                 </table>
                 
@@ -445,24 +332,12 @@ const Protocolos = () => {
             </div>
           </div>
         </div>
-<<<<<<< HEAD
-      </div>
-      {showEditModal && (
-        <EditModal
-          setShowEditModal={setShowEditModal}
-          razon={currentData.razon}
-          direccion={currentData.direccion}
-          telefono={currentData.telefono}
-          responsable={currentData.responsable}
-          email={currentData.email}
-=======
     </div>
       {loading && <Loading/>}
       {showEditModal && (
         <EditModal 
           setShowEditModal={setShowEditModal} 
 
->>>>>>> 0d90639f776ffc7c7e18d9bc3370e30213eb5697
         />
       )}
       {showModalRegistroServicios && <ModalRegistroServicios setShowModalRegistroServicios={setShowModalRegistroServicios} user={UserLogued.sub} token={token}  />}
