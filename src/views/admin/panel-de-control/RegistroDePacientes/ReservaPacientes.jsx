7f3221dedc5b0d1b533bd3bff774708a705005e5
repoHM <sistemas@horizontemedic.com>
2024-07-15 12:37:34 -0500

    delete filters

--- src/views/admin/panel-de-control/RegistroDePacientes/ReservaPacientes.jsx
+++ src/views/admin/panel-de-control/RegistroDePacientes/ReservaPacientes.jsx
@@ -31,49 +31,17 @@ const ReservaPacientes = ({ selectedSede, token, userlogued }) => {
     setSearchTerm(event.target.value);
   };
 
-  const handleFilterChange = (event) => {
-    setFilterSede(event.target.value);
-  };
-
-  const toggleSortDirection = () => {
-    setSortDirection(sortDirection === 'ascending' ? 'descending' : 'ascending');
-  };
 
   const closeModal = () => {
     setShowModal(false);
     setSelectedUser(null);
   };
-
   // Filtrar y ordenar las reservas
-  let filteredReservations = reservations.filter((reservation) =>
-    reservation.usuario.toLowerCase().includes(searchTerm.toLowerCase()) &&
-    (filterSede === '' || reservation.sede.toLowerCase() === filterSede.toLowerCase())
-  );
 
-  // Ordenar según la dirección de ordenamiento
-  filteredReservations.sort((a, b) => {
-    if (sortDirection === 'ascending') {
-      return a.cantidad - b.cantidad;
-    } else {
-      return b.cantidad - a.cantidad;
-    }
-  });
 
   return (
     <div className="overflow-x-auto" style={{ maxHeight: '500px' }}>
-      <div className="flex space-x-2 mb-3">
-        <input
-          type="text"
-          placeholder="Buscar por usuario..."
-          value={searchTerm}
-          onChange={handleSearchChange}
-          className="border border-gray-300 rounded-md px-3 py-2"
-        />
-        <button onClick={toggleSortDirection} className="border border-gray-300 rounded-md px-3 py-2 flex items-center">
-          <span style={{ marginRight: '0.5rem' }}>Ordenar por cantidad</span>
-          {sortDirection === 'ascending' ? <FontAwesomeIcon icon={faSortAmountUp} /> : <FontAwesomeIcon icon={faSortAmountDown} />}
-        </button>
-      </div>
+
 
       <table className="min-w-full bg-white">
         <thead>
@@ -87,12 +55,8 @@ const ReservaPacientes = ({ selectedSede, token, userlogued }) => {
           </tr>
         </thead>
         <tbody>
-          {filteredReservations.length === 0 ? (
-            <tr>
-              <td colSpan="6" className="px-4 py-2 border text-center">No se encontraron reservas registradas con este usuario / usuario no existe.</td>
-            </tr>
-          ) : (
-            filteredReservations.map((reservation, index) => (
+          {
+            reservations.map((reservation, index) => (
               <tr key={index}>
                 <td className="px-4 py-2 border">{reservation.usuario}</td>
                 <td className="px-4 py-2 border">{reservation.sede}</td>
@@ -109,7 +73,7 @@ const ReservaPacientes = ({ selectedSede, token, userlogued }) => {
                 </td>
               </tr>
             ))
-          )}
+          }
         </tbody>
       </table>
 
