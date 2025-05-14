09a5b3e802fff9bf996f411e183c6af3118b8e27
Daniel Plasencia <daniel.plasencia.dlc@gmail.com>
2025-05-14 14:12:14 -0500

    url correcion xd

--- src/views/config/config.js
+++ src/views/config/config.js
@@ -1,5 +1,5 @@
 //Este archivo sirve para poder cambiar de rutas para las apis
 
-export const URLAzure = 'https://testbackendhm.azurewebsites.net'
+export const URLAzure = 'https://hmintegracion.azurewebsites.net'
 export const URLAPIREST = 'http://go.server.net.pe:3000/api/v2'
 export const TokenURL = 'horizontemedic_dtcm9'
