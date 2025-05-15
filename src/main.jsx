e8c96db5276c8fe3e8b25b5ea82eecbfe1b10908
Jmmsimon <jeansimon176@gmail.com>
2025-05-15 06:35:40 -0500

    cambios de dani

--- src/main.jsx
+++ src/main.jsx
@@ -33,7 +33,7 @@ import Formulario from './views/admin/RegistroUnico/Formulario.jsx'
 //Registro de Pacientes Multiple
 import RegistroDePacientes from './views/admin/panel-de-control/RegistroDePacientes/Registro-de-pacientes.jsx';
 //jaspers
-import Sucamec from './views/jaspers/ReporteExamen1.jsx';
+import Sucamec from './views/jaspers/AnalisisBioquimicos.jsx';
 
 const App = () => {
   return (
