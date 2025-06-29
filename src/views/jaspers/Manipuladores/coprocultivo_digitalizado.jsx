0cacb70357a979917588d7ca4c2bbc24d601812c
repoHM <sistemas@horizontemedic.com>
2025-06-28 23:19:59 -0500

    otra vez sabado 28 xd

--- src/views/jaspers/Manipuladores/coprocultivo_digitalizado.jsx
+++ src/views/jaspers/Manipuladores/coprocultivo_digitalizado.jsx
@@ -9,6 +9,7 @@ export default function Coprocultivo_Digitalizado(datos = {}) {
   const margin = 15;
 
   
+  
   // === HEADER ===
   headerCoprocultivoDigitalizado(doc, datos);
   const sello1 = datos.digitalizacion?.find(d => d.nombreDigitalizacion === "SELLOFIRMA");
