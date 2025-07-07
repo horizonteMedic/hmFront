345bfd6fd69e663693b101b51e6ab5414ef4d700
Piero Villanueva <pierofabrizzio9@hotmail.com>
2025-07-07 15:30:50 -0500

    Pv Subida emergencia

--- src/views/admin/panel-de-control/SistemaOcupacional/Audiometria/AudiometriaOhla/AudiometriaOhla.jsx
+++ src/views/admin/panel-de-control/SistemaOcupacional/Audiometria/AudiometriaOhla/AudiometriaOhla.jsx
@@ -72,6 +72,11 @@ export default function AudiometriaOhla({ token, selectedSede, userlogued }) {
     perdida_global: "",
     asignar_especialista: false,
 
+    //derecha
+    nombres_search: "",
+    codigo_search: "",
+    diagnostico: "",
+
     //antiguos
     sordera: "NO",
     acufenos: "NO",
@@ -137,7 +142,7 @@ export default function AudiometriaOhla({ token, selectedSede, userlogued }) {
     empresa: "",
     contrata: "",
   };
-
+  const [exams, setExams] = useState([]);
   const [form, setForm] = useState(initialFormState);
   const [status, setStatus] = useState("");
 
@@ -273,9 +278,9 @@ export default function AudiometriaOhla({ token, selectedSede, userlogued }) {
   };
 
   return (
-    <div className="w-full max-w-[90vw] lg:max-w-[70vw] mx-auto bg-white rounded shadow p-6 space-y-6">
+    <div className="w-full max-w-[90vw]  mx-auto bg-white rounded shadow p-6 space-y-6">
       <h2 className="text-2xl font-bold text-center mb-6">AUDIOMETRIA OHLA</h2>
-      <div className="grid grid-cols-1 xl:grid-cols-2 gap-6">
+      <div className="grid grid-cols-1 lg:grid-cols-[2.5fr_3fr] gap-6">
         {/* Lado izquierdo */}
         <div className="border rounded p-4 mt-6 flex flex-col gap-4">
           {/* Encabezado */}
@@ -624,21 +629,20 @@ export default function AudiometriaOhla({ token, selectedSede, userlogued }) {
           </div>
         </div>
         {/* Lado derecho */}
-          <div className="border rounded p-4 mt-6 flex flex-col gap-4">
-          {/* Encabezado */}
+        <div className="border rounded p-4 mt-6 flex flex-col gap-4">
+          {/* Search */}
 
           <div className="flex flex-col 2xl:flex-row gap-4">
             <div className="flex-1 flex items-center gap-2">
               <label className="font-semibold min-w-[50px] md:min-w-[90px] text-base">
-                Nro Ficha:
+                Nombres:
               </label>
               <input
-                name="norden"
-                value={form.norden}
+                name="nombres_search"
+                value={form.nombres_search}
                 onKeyUp={(e) => {
                   if (e.key === "Enter") {
-                    handleClearnotO();
-                    VerifyTR(form.norden, tabla, token, setForm, selectedSede);
+                    //search
                   }
                 }}
                 onChange={handleChange}
@@ -646,233 +650,54 @@ export default function AudiometriaOhla({ token, selectedSede, userlogued }) {
               />
             </div>
             <div className="flex-1 flex items-center gap-2">
-              <label className="font-semibold text-base min-w-[50px]">
-                Fecha:
+              <label className="font-semibold min-w-[50px] md:min-w-[90px] text-base">
+                Código:
               </label>
               <input
-                type="date"
-                name="fecha"
-                value={form.fecha}
+                name="codigo_search"
+                value={form.codigo_search}
+                onKeyUp={(e) => {
+                  if (e.key === "Enter") {
+                    //search
+                  }
+                }}
                 onChange={handleChange}
                 className="border rounded px-2 py-1 text-base flex-1"
               />
             </div>
           </div>
-          {/* Paciente */}
-          <div className="flex flex-col 2xl:flex-row gap-4">
-            <div className="flex-1 flex items-center gap-2">
-              <label className="font-semibold min-w-[50px] md:min-w-[90px] text-base">
-                DNI:
-              </label>
-              <input
-                name="dni"
-                value={form.dni}
-                disabled
-                className="border rounded px-2 py-1 text-base flex-1 bg-gray-100"
-              />
-            </div>
-            <div className="flex-1 flex items-center gap-4">
-              <label className="font-semibold min-w-[50px] text-base">
-                Fecha de Nacimiento:
-              </label>
-              <input
-                name="fechaNac"
-                value={form.fechaNac}
-                disabled
-                className="border rounded px-2 py-1 text-base flex-1 bg-gray-100"
-              />
-            </div>
-          </div>
-
-          <div className="flex-1 flex items-center gap-2">
-            <label className="font-semibold min-w-[50px] md:min-w-[90px] text-base">
-              Nombres:
-            </label>
-            <input
-              name="nombres"
-              value={form.nombres}
-              disabled
-              className="border rounded px-2 py-1 text-base flex-1 bg-gray-100"
+          <div className="flex-1">
+            <Table
+              data={exams}
+              tabla={tabla}
+              set={setForm}
+              token={token}
+              clean={handleClear}
             />
           </div>
-          <div className="flex-1 flex items-center gap-2">
-            <label className="font-semibold min-w-[50px] md:min-w-[90px] text-base">
-              Edad:
-            </label>
-            <input
-              name="edad"
-              value={form.edad}
-              disabled
-              className="border rounded px-2 py-1 text-base flex-1 bg-gray-100"
-            />
-          </div>
-          <div className="flex-1 flex items-center gap-2">
-            <label className="font-semibold min-w-[50px] md:min-w-[90px] text-base">
-              Ex. Médico:
-            </label>
-            <input
-              name="nomExam"
-              value={form.nomExam}
-              disabled
-              className="border rounded px-2 py-1 text-base flex-1 bg-gray-100"
-            />
-          </div>
-
-          <div className="h-0.5 w-full bg-gray-200 rounded"></div>
-          <div className="flex-1 flex items-center gap-2">
-            <label className="font-semibold min-w-[50px] md:min-w-[90px] text-base">
-              Empresa:
-            </label>
-            <input
-              name="empresa"
-              value={form.empresa}
-              disabled
-              className="border rounded px-2 py-1 text-base flex-1 bg-gray-100"
-            />
-          </div>
-          <div className="flex-1 flex items-center gap-2">
-            <label className="font-semibold min-w-[50px] md:min-w-[90px] text-base">
-              Contrata:
-            </label>
-            <input
-              name="contrata"
-              value={form.contrata}
-              disabled
-              className="border rounded px-2 py-1 text-base flex-1 bg-gray-100"
-            />
-          </div>
-          {/* end  Encabezado */}
-          <div className="flex justify-end gap-4 py-1 pr-4">
-            <label className="flex items-center gap-2 select-none">
-              <input
-                type="checkbox"
-                name="no_paso_Examen"
-                checked={form.no_paso_Examen}
-                onChange={() => {
-                  toggleCheckBox("no_paso_Examen");
+          <div className="grid grid-cols-1  gap-4 border rounded p-4">
+            <div className="flex justify-end items-center ">
+              <button
+                type="button"
+                onClick={() => {
+                  // calcularOidos();
                 }}
-              />
-              No Paso Examen
-            </label>
-            <label className="flex items-center gap-2 select-none">
-              <input
-                type="checkbox"
-                name="activar_grafico"
-                checked={form.activar_grafico}
-                onChange={() => {
-                  toggleCheckBox("activar_grafico");
-                }}
-              />
-              Activar Gráfico
-            </label>
-          </div>
-          {/* Audiometría  Área  */}
-          <div className="grid grid-cols-1 gap-8 border rounded p-4 ">
-            <h4 className="font-semibold text-lg ">Audiometría Área:</h4>
-            {/* Oído Derecho */}
-            <div className="flex flex-col p-4 border rounded items-center justify-center">
-              <h4 className="font-medium mb-2 w-full">Oído Derecho</h4>
-              <div className="grid grid-cols-8 gap-1 text-center text-sm font-semibold ">
-                <div className="flex flex-col items-start  gap-2">
-                  <p>hz</p>
-                  <p>dB (A)</p>
-                </div>
-                {frecuencias.map((hz) => (
-                  <div key={hz}>
-                    <p>{hz}</p>
-                    <input
-                      type="text"
-                      name={`od_${hz}`}
-                      value={form[`od_${hz}`] || ""}
-                      onChange={handleChangeNumber}
-                      // placeholder="dB"
-                      className="border rounded px-1 py-1 text-center w-full"
-                      onKeyDown={(e) => {
-                        if (e.key === "Enter") {
-                          e.preventDefault();
-                          document
-                            .getElementsByName(
-                              `${getNextArrayItem(
-                                "od_",
-                                hz,
-                                "",
-                                frecuencias,
-                                "oi_500"
-                              )}`
-                            )?.[0]
-                            ?.focus();
-                        }
-                      }}
-                    />
-                  </div>
-                ))}
-              </div>
+                className="bg-blue-500 hover:bg-blue-600 text-white text-base px-6 py-2 rounded flex items-center gap-2 w-full  max-w-[120px]"
+              >
+                <FontAwesomeIcon icon={faStethoscope} /> Diagnosticar
+              </button>
             </div>
 
-            {/* Oído Izquierdo */}
-            <div className="flex flex-col p-4 border rounded items-center justify-center">
-              <h4 className="font-medium mb-2 w-full">Oído Izquierdo</h4>
-              <div className="grid grid-cols-8 gap-1 text-center text-sm font-semibold">
-                <div className="flex flex-col items-start gap-2">
-                  <p>hz</p>
-                  <p>dB (A)</p>
-                </div>
-                {frecuencias.map((hz) => (
-                  <div key={hz}>
-                    <p>{hz}</p>
-                    <input
-                      type="text"
-                      name={`oi_${hz}`}
-                      value={form[`oi_${hz}`] || ""}
-                      onChange={handleChangeNumber}
-                      // placeholder="dB"
-                      className="border rounded px-1 py-1 text-center w-full"
-                      onKeyDown={(e) => {
-                        if (e.key === "Enter") {
-                          e.preventDefault();
-                          document
-                            .getElementsByName(
-                              `${getNextArrayItem(
-                                "oi_",
-                                hz,
-                                "",
-                                frecuencias,
-                                ""
-                              )}`
-                            )?.[0]
-                            ?.focus();
-                        }
-                      }}
-                    />
-                  </div>
-                ))}
-              </div>
-            </div>
-          </div>
-          
-          <div className="flex flex-row gap-4 pr-4">
-            <div className="flex-1 flex items-center gap-2">
-              <label className="font-semibold min-w-[50px] md:min-w-[90px] text-base">
-                % pérdida global:
-              </label>
-              <input
-                name="perdida_global"
-                value={form.perdida_global}
-                disabled
-                className="border rounded px-2 py-1 text-base flex-1 bg-gray-100"
+            <div>
+              <label className="block font-medium mb-1">Diagnóstico:</label>
+              <textarea
+                name="diagnostico"
+                value={form.diagnostico || ""}
+                onChange={handleChange}
+                className="w-full border rounded px-2 py-1"
+                rows={3}
               />
             </div>
-            <label className="flex items-center gap-2 select-none">
-              <input
-                type="checkbox"
-                name="asignar_especialista"
-                checked={form.asignar_especialista}
-                onChange={() => {
-                  toggleCheckBox("asignar_especialista");
-                }}
-              />
-              Asignar Especialista
-            </label>
           </div>
         </div>
       </div>
@@ -921,6 +746,80 @@ export default function AudiometriaOhla({ token, selectedSede, userlogued }) {
       {status && (
         <p className="mt-4 text-center text-green-600 text-base">{status}</p>
       )}
+    </div>
+  );
+}
+
+function Table({ data, tabla, set, token, clean }) {
+  // confirmación antes de imprimir
+  const handlePrintConfirm = (nro) => {
+    Swal.fire({
+      title: "Confirmar impresión",
+      text: `¿Deseas imprimir la ficha Nº ${nro}?`,
+      icon: "question",
+      showCancelButton: true,
+      confirmButtonText: "Sí, imprimir",
+      cancelButtonText: "No",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        PrintHojaR(nro, tabla, token);
+      }
+    });
+  };
+
+  function clicktable(nro) {
+    clean();
+    Loading("Importando Datos");
+    // GetInfoPacAnalisisBio(nro, tabla, set, token, setMed);
+  }
+
+  return (
+    <div className="overflow-y-auto" style={{ maxHeight: "calc(12 * 4rem)" }}>
+      <table className="w-full table-auto border-collapse">
+        <thead>
+          <tr className="bg-gray-100">
+            <th className="border px-2 py-1 text-left text-lg">Cod.</th>
+            <th className="border px-2 py-1 text-left text-lg">N° Orden</th>
+            <th className="border px-2 py-1 text-left text-lg">Nombres</th>
+            <th className="border px-2 py-1 text-left text-lg">Fecha</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.length > 0 ? (
+            data.map((row, i) => (
+              <tr
+                key={i}
+                className={`hover:bg-gray-50 cursor-pointer text-lg ${
+                  row.color === "AMARILLO"
+                    ? "bg-[#ffff00]"
+                    : row.color === "VERDE"
+                    ? "bg-[#00ff00]"
+                    : "bg-[#ff6767]"
+                }`}
+                onClick={() => clicktable(row.n_orden)}
+                onContextMenu={(e) => {
+                  e.preventDefault();
+                  handlePrintConfirm(row.n_orden);
+                }}
+              >
+                <td className="border px-2 py-1 font-bold">{row.codigo}</td>
+                <td className="border px-2 py-1">{row.n_orden}</td>
+                <td className="border px-2 py-1">{row.nombres}</td>
+                <td className="border px-2 py-1">{row.fecha}</td>
+              </tr>
+            ))
+          ) : (
+            <tr>
+              <td
+                colSpan={5}
+                className="text-center py-4 text-gray-500 text-lg"
+              >
+                No hay datos
+              </td>
+            </tr>
+          )}
+        </tbody>
+      </table>
     </div>
   );
 }
