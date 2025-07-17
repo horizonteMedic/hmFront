b84000308e65132b63c87087606fa81ecc15b4ad
PVillanueva <pierofabrizzio9@hotmail.com>
2025-07-17 13:47:39 -0500

    Pv cambios OH y reporte

--- src/views/admin/panel-de-control/SistemaOcupacional/HistoriaOcupacional/controller/controllerHO.jsx
+++ src/views/admin/panel-de-control/SistemaOcupacional/HistoriaOcupacional/controller/controllerHO.jsx
@@ -44,6 +44,7 @@ export const VerifyTR = async (nro,tabla,token,set,sede,setTable) => {
         console.log(res)
         if (res.id === 0) {
             GetInfoPac(nro,set,token,sede)
+          
             GetInfoAnterior(nro,token,setTable)
         } else {
             GetInfoHistoriaOcupacinal(nro,tabla,set,token,setTable)
@@ -55,18 +56,18 @@ export const GetInfoAnterior = (nro,token,setTable) => {
   getFetch(`/api/v01/ct/historiaOcupacional/obtenerHistoriaOcupacionalDetallesPorNorden?nOrden=${nro}`,token)
   .then((res) => {
     if (res&&res.length > 0) {
+       Swal.fire('Info', 'Este norden no cuenta con historia ocupacional','info')
         console.log(res)
       const detallesOrdenados = res.sort(
         (a, b) => getAñoInicial(a.fecha) - getAñoInicial(b.fecha)
       );
-    
+      
       setTable(detallesOrdenados)
+     
     } else {
-      Swal.fire('Error', 'Ocurrio un error al traer los datos','error')
+      Swal.fire('Info', 'Este norden no tiene registros de historia ocupacional previos','info')
+
     }
-  })
-  .finally(() => {
-    Swal.close()
   })
 }
 
@@ -80,9 +81,7 @@ export const GetInfoPac = (nro,set,token,sede) => {
         nombres: res.nombresApellidos,
         }));
     })
-    .finally(() => {
-      Swal.close()
-    })
+    
 }
 
 const getAñoInicial = (fecha) => {
--- src/views/jaspers/HistoriaOcupacional/Header/HistoriaOcupacionalHeader.jsx
+++ src/views/jaspers/HistoriaOcupacional/Header/HistoriaOcupacionalHeader.jsx
@@ -118,7 +118,7 @@ const header_HistoriaOcupacional = (doc, datos = {}) => {
       { label: "Fecha de Nacimiento :", value: datos.fechaNac || '' },
     ],
     [
-      { label: "Profesion :", value: datos.areaO || '' },
+      { label: "Profesion :", value: datos.profesion || '' },
       { label: "Sexo :", value: datos.sexo === 'F' ? 'FEMENINO' : datos.sexo === 'M' ? 'MASCULINO' : '' },
     ],
     [
