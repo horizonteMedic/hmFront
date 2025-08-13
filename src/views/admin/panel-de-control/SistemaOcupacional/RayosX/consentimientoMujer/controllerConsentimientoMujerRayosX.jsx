1fed8a4b21ca17bfa55d0c7c91552f729bf2e59f
Piero Villanueva <pierofabrizzio9@hotmail.com>
2025-08-13 14:34:47 -0500

    Pv cambios consentimiento rayos x

---
+++ src/views/admin/panel-de-control/SistemaOcupacional/RayosX/consentimientoMujer/controllerConsentimientoMujerRayosX.jsx
@@ -0,0 +1,187 @@
+import Swal from "sweetalert2";
+import { getFetch } from "../../../getFetch/getFetch";
+import { SubmitData } from "../model";
+
+const obtenerReporteUrl =
+  "/api/v01/ct/rayosX/obtenerReporteConsentimientoRayosX";
+const registrarUrl =
+  "/api/v01/ct/rayosX/registrarActualizarConsentimientoRayosX";
+
+export const Loading = (text) => {
+  Swal.fire({
+    title: `<span style="font-size:1.3em;font-weight:bold;">${text}</span>`,
+    html: `<div style="font-size:1.1em;"><span style='color:#0d9488;font-weight:bold;'></span></div><div class='mt-2'>Espere por favor...</div>`,
+    icon: "info",
+    background: "#f0f6ff",
+    color: "#22223b",
+    showConfirmButton: false,
+    allowOutsideClick: false,
+    allowEscapeKey: false,
+    showCancelButton: true,
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Yes, delete it!",
+    customClass: {
+      popup: "swal2-border-radius",
+      title: "swal2-title-custom",
+      htmlContainer: "swal2-html-custom",
+    },
+    showClass: {
+      popup: "animate__animated animate__fadeInDown",
+    },
+    hideClass: {
+      popup: "animate__animated animate__fadeOutUp",
+    },
+    didOpen: () => {
+      Swal.showLoading();
+    },
+  });
+};
+
+const convertirFecha = (fecha) => {
+  if (fecha === "") return "";
+  const [dia, mes, anio] = fecha.split("-");
+  return `${anio}/${mes.padStart(2, "0")}/${dia.padStart(2, "0")}`;
+};
+
+export const GetInfoPac = (nro, set, token, sede) => {
+  getFetch(
+    `/api/v01/ct/infoPersonalPaciente/busquedaPorFiltros?nOrden=${nro}&nomSede=${sede}`,
+    token
+  )
+    .then((res) => {
+      console.log("pros", res);
+      if (res.norden) {
+        set((prev) => ({
+          ...prev,
+          ...res,
+          nombres: res.nombresApellidos,
+          ocupacion: res.cargo,
+        }));
+      }
+    })
+    .finally(() => {
+      Swal.close();
+    });
+};
+
+export const GetInfoServicio = (
+  nro,
+  tabla,
+  set,
+  token,
+  onFinish = () => {}
+) => {
+  getFetch(`${obtenerReporteUrl}?nOrden=${nro}&nameService=${tabla}`, token)
+    .then((res) => {
+      console.log(res);
+      set((prev) => ({
+        ...prev,
+        ...res,
+      }));
+    })
+    .finally(() => {
+      onFinish();
+    });
+};
+
+export const VerifyTR = async (nro, tabla, token, set, sede) => {
+  if (!nro) {
+    await Swal.fire(
+      "Error",
+      "Debe Introducir un Nro de Historia Clinica válido",
+      "error"
+    );
+    return;
+  }
+  Loading("Validando datos");
+  getFetch(
+    `/api/v01/ct/consentDigit/existenciaExamenes?nOrden=${nro}&nomService=${tabla}`,
+    token
+  ).then((res) => {
+    console.log(res);
+    if (res.id === 0) {
+      //No tiene registro previo
+      GetInfoPac(nro, set, token, sede);
+    } else if (res.id === 1) {
+      // Ya existe registro
+      GetInfoServicio(nro, tabla, set, token, () => {
+        Swal.fire(
+          "Alerta",
+          "Este paciente ya cuenta con registros de Consentimiento.",
+          "warning"
+        );
+      });
+    }
+  });
+};
+
+export const SubmitDataService = async (form, token, user, limpiar, tabla) => {
+  if (!form.norden) {
+    await Swal.fire("Error", "Datos Incompletos", "error");
+    return;
+  }
+  Loading("Registrando Datos");
+  const currentTime = new Date().toLocaleTimeString("en-US", {
+    hour12: false,
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+  });
+  const body = {
+    norden: form.norden,
+    fecha: form.fecha,
+    hora: currentTime,
+    userRegistro: user,
+  };
+  SubmitData(body, registrarUrl, token).then((res) => {
+    console.log(res);
+    if (res.id === 1 || res.id === 0) {
+      Swal.fire({
+        title: "Exito",
+        text: `${res.mensaje},\n¿Desea imprimir?`,
+        icon: "success",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+      }).then((result) => {
+        limpiar();
+        if (result.isConfirmed) {
+          PrintHojaR(form.norden, token, tabla);
+        }
+      });
+    } else {
+      Swal.fire("Error", "Ocurrio un error al Registrar", "error");
+    }
+  });
+};
+
+export const PrintHojaR = (nro, token, tabla) => {
+  Loading("Cargando Formato a Imprimir");
+  getFetch(
+    `${obtenerReporteUrl}?nOrden=${nro}&nameService=${tabla}`,
+    token
+  ).then(async (res) => {
+    if (res.norden) {
+      console.log(res);
+      const nombre = res.nameJasper;
+      console.log(nombre);
+      const jasperModules = import.meta.glob(
+        "../../../../../jaspers/RayosX/consentimientoMujer/*.jsx"
+      );
+      const modulo = await jasperModules[
+        `../../../../../jaspers/RayosX/consentimientoMujer/${nombre}.jsx`
+      ]();
+      // Ejecuta la función exportada por default con los datos
+      if (typeof modulo.default === "function") {
+        modulo.default(res);
+      } else {
+        console.error(
+          `El archivo ${nombre}.jsx no exporta una función por defecto`
+        );
+      }
+      Swal.close();
+    } else {
+      Swal.close();
+    }
+  });
+};
