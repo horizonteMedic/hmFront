e2b14bb47aadf1f4e9ea881ea80c8cc783fb4ac5
Daniel Plasencia <daniel.plasencia.dlc@gmail.com>
2025-08-13 15:03:49 -0500

    dani correciones consentimiento

--- src/views/admin/panel-de-control/SistemaOcupacional/Cuestionario_Nordico/Cuestionario_Nordico.jsx
+++ src/views/admin/panel-de-control/SistemaOcupacional/Cuestionario_Nordico/Cuestionario_Nordico.jsx
@@ -281,27 +281,46 @@ const Cuestionario_Nordico = ({ token, selectedSede, userlogued, userDatos }) =>
       <div className="max-w-[70%] mx-auto">
         <h1 className="text-3xl font-bold mb-4 text-center">Cuestionario Nórdico de Signos y Síntomas Osteomusculares</h1>
         {/* Tabs */}
-        <div className="flex space-x-1 overflow-x-auto mt-4">
-          {tabs.map((tab, idx) => (
-            <button
-              key={idx}
-              onClick={() => setActiveTab(idx)}
-              className={`px-6 py-2 border rounded-t-lg transition duration-150 text-base font-semibold focus:outline-none flex items-center whitespace-nowrap ${
-                activeTab === idx
-                  ? "bg-[#233245] text-white font-bold"
-                  : "bg-gray-100 text-gray-700 hover:bg-gray-200"
-              }`}
-            >
-              <FontAwesomeIcon icon={tab.icon} className="mr-2" />
-              {tab.label}
-            </button>
-          ))}
+        <div className="flex flex-col space-x-1 mt-4 border shadow p-8 mx-auto">
+           <Cuestionario
+              token={token}
+              selectedSede={selectedSede}
+              userlogued={userlogued}
+              form={form}
+              setForm={setForm}
+            />
+            <Responder
+              token={token}
+              selectedSede={selectedSede}
+              userlogued={userlogued}
+              form={form}
+              setForm={setForm}
+            />
+            <Espalda_Baja
+              token={token}
+              selectedSede={selectedSede}
+              userlogued={userlogued}
+              form={form}
+              setForm={setForm}
+            />
+            <Hombros
+              token={token}
+              selectedSede={selectedSede}
+              userlogued={userlogued}
+              form={form}
+              setForm={setForm}
+            />
+            <Cuello
+              token={token}
+              selectedSede={selectedSede}
+              userlogued={userlogued}
+              form={form}
+              setForm={setForm}
+            />
+
         </div>
 
-        {/* Active Content */}
-        <div className="border border-gray-200 border-t-0 p-4 bg-white rounded-b-lg text-lg">
-          {tabs[activeTab].component}
-        </div>
+
 
       </div>
     </div>
