cb8b1db38d0322d908e24daf68795b3635b28495
Piero Villanueva <pierofabrizzio9@hotmail.com>
2025-11-03 14:11:23 -0500

    Pv anexo 16 y psicologia

--- src/views/admin/panel-de-control/SistemaOcupacional/ModuloPsicologia/PsicologiaTabSelector.jsx
+++ src/views/admin/panel-de-control/SistemaOcupacional/ModuloPsicologia/PsicologiaTabSelector.jsx
@@ -1,56 +1,28 @@
 import TabSelector from "../../../../components/reusableComponents/TabSelector";
-import InformePsicologico from "./InformePsicologico/InformePsicologico";
-import FichaPsicologica2 from "./FichaPsicologica2/FichaPsicologica2";
-import FichaPsicologica3 from "./FichaPsicologica3/FichaPsicologica3";
-import InformePsicolaboral from "./InformePsicolaboral/InformePsicolaboral";
-import InformePsicologicoADECO from "./InformePsicologicoADECO/InformePsicologicoADECO";
-import ExamenEspacioConfinado from "./ExamenEspacioConfinado/ExamenEspacioConfinado";
-import EvaluacionPsicologicaPoderosa from "./EvaluacionPsicologicaPoderosa/EvaluacionPsicologicaPoderosa";
+import AnexosPsicologiaSubSelector from "./AnexosPsicologiaSubSelector";
+import PsicologiaBorooSubSelector from "./PsicologiaBorooSubSelector";
+import ExamenesComplementariosSubSelector from "./ExamenesComplementariosSubSelector";
 
 export default function PsicologiaTabSelector({ tieneVista }) {
     const tabsConfig = [
         {
             id: 0,
-            permission: "Informe Psicologico",
-            label: "Informe Psicologico",
-            component: InformePsicologico
+            permission: "Anexos Psicologia",
+            label: "Anexos Psicologia",
+            component: AnexosPsicologiaSubSelector
         },
         {
             id: 1,
-            permission: "Ficha Psicologica 2",
-            label: "Ficha Psicológica 2",
-            component: FichaPsicologica2
+            permission: "Psicologia Boroo",
+            label: "Psicologia Boroo",
+            component: PsicologiaBorooSubSelector
         },
         {
             id: 2,
-            permission: "Ficha Psicologica 3",
-            label: "Ficha Psicológica 3",
-            component: FichaPsicologica3
+            permission: "Examenes Complementarios Psicologia",
+            label: "Examenes Complementarios Psicologia",
+            component: ExamenesComplementariosSubSelector
         },
-        {
-            id: 3,
-            permission: "Informe Psicolaboral",
-            label: "Informe Psicolaboral",
-            component: InformePsicolaboral
-        },
-        {
-            id: 4,
-            permission: "Informe Psicologico ADECO",
-            label: "Informe Psicológico ADECO",
-            component: InformePsicologicoADECO
-        },
-        {
-            id: 5,
-            permission: "Examen Espacio Confinado",
-            label: "Examen Espacio Confinado",
-            component: ExamenEspacioConfinado
-        },
-        {
-            id: 6,
-            permission: "Examen Espacio Confinado",
-            label: "Evaluacion Psicologica Poderosa",
-            component: EvaluacionPsicologicaPoderosa
-        }
     ];
     return (
         <TabSelector
---
+++ src/views/components/reusableComponents/SubTabSelector.jsx
@@ -0,0 +1,51 @@
+import { useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+export default function TabSelector({ tieneVista, tabsConfig }) {
+    const [activeTab, setActiveTab] = useState(0);
+
+    useEffect(() => {
+        // Find the first available tab based on permissions
+        const firstAvailableTab = tabsConfig.find(tab => tieneVista(tab.permission));
+        setActiveTab(firstAvailableTab ? firstAvailableTab.id : -1);
+    }, []);
+
+    // Get available tabs based on permissions
+    const availableTabs = tabsConfig.filter(tab => tieneVista(tab.permission));
+
+    // Get active tab component
+    const ActiveComponent = tabsConfig.find(tab => tab.id === activeTab)?.component;
+
+    return (
+        <div className="space-y-4">
+            <div className="bg-white ">
+                <div className="flex space-x-1 overflow-x-auto mb-4">
+                    {availableTabs.map(tab => (
+                        <button
+                            key={tab.id}
+                            onClick={() => setActiveTab(tab.id)}
+                            className={`px-6 py-2 border rounded-t-lg transition duration-150 text-base font-semibold focus:outline-none flex items-center whitespace-nowrap ${activeTab === tab.id
+                                ? 'bg-[#233245] text-white font-bold'
+                                : 'bg-gray-100 text-gray-700 hover:bg-gray-200'
+                                }`}
+                        >
+                            {tab.icon && (
+                                <FontAwesomeIcon icon={tab.icon} className="mr-2" />
+                            )}
+                            {tab.label}
+                        </button>
+                    ))}
+                </div>
+                {/* Tab Content */}
+                <div className="max-w-full">
+                    {ActiveComponent && <ActiveComponent />}
+                    {activeTab === -1 && (
+                        <div className="text-center text-gray-500">
+                            No tiene permisos para ver ningún examen.
+                        </div>
+                    )}
+                </div>
+            </div>
+        </div>
+    );
+}
