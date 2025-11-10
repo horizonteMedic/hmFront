2466decee3e9b5a3bfc6a981510af7c9c13e1a5a
Jmmsimon <jeansimon176@gmail.com>
2025-11-10 10:50:10 -0500

    informe adeco

--- src/main.jsx
+++ src/main.jsx
@@ -45,7 +45,7 @@ import Anexo16A from "./views/admin/panel-de-control/SistemaOcupacional/Anexo16A
 
 
 //SIMULADOR DE jaspers
-import Ficha from "./views/jaspers/Poderosa/CertificadoMedicoManipuladores_Barrick_Digitalizado.jsx"
+import Ficha from "./views/jaspers/ModuloPsicologia/InformePsicologicoADECO/InformePsicologicoAdecoEstres_Digitalizado.jsx"
 
 
 
