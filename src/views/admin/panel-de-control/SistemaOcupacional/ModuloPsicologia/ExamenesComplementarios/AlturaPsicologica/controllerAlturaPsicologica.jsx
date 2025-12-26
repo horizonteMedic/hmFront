d16299081492d9de430816e8c7b63843006fc9f3
PVillanueva <pierofabrizzio9@hotmail.com>
2025-12-26 11:10:07 -0500

    Pv Altura psicologica

--- src/views/admin/panel-de-control/SistemaOcupacional/ModuloPsicologia/ExamenesComplementarios/AlturaPsicologica/controllerAlturaPsicologica.jsx
+++ src/views/admin/panel-de-control/SistemaOcupacional/ModuloPsicologia/ExamenesComplementarios/AlturaPsicologica/controllerAlturaPsicologica.jsx
@@ -11,9 +11,9 @@ import { formatearFechaCorta } from "../../../../../../utils/formatDateUtils";
 
 // Reutilizamos los endpoints generales de Informe Psicológico
 const obtenerReporteUrl =
-  "";
+  "/api/v01/ct/informePsicologicoFobias/obtenerReporteInformePsicologicoFobias";
 const registrarUrl =
-  "";
+  "/api/v01/ct/informePsicologicoFobias/registrarActualizarInformePsicologicoFobias";
 
 export const GetInfoServicio = async (
   nro,
@@ -33,7 +33,75 @@ export const GetInfoServicio = async (
     set((prev) => ({
       ...prev,
       norden: res.norden,
+      fecha: res.fecha,
 
+      esApto: res.apto ?? false,
+
+      nombreExamen: res.nombreExamenPsicologico ?? "",
+      dni: res.dniPaciente ?? "",
+
+      nombres: `${res.nombresPaciente ?? ""} ${res.apellidosPaciente ?? ""}`,
+      fechaNacimiento: formatearFechaCorta(res.fechaNacimientoPaciente ?? ""),
+      lugarNacimiento: res.lugarNacimientoPaciente ?? "",
+      edad: res.edadPaciente ?? "",
+      sexo: res.sexoPaciente === "M" ? "MASCULINO" : "FEMENINO",
+      estadoCivil: res.estadoCivilPaciente ?? "",
+      nivelEstudios: res.nivelEstudioPaciente ?? "",
+      // Datos Laborales
+      empresa: res.empresa ?? "",
+      contrata: res.contrata ?? "",
+      ocupacion: res.ocupacionPaciente ?? "",
+      cargoDesempenar: res.cargoPaciente ?? "",
+
+      razonamiento: res.razonamientoI ? "I" :
+        res.razonamientoNPI ? "NPI" :
+          res.razonamientoNP ? "NP" :
+            res.razonamientoNPS ? "NPS" :
+              res.razonamientoS ? "S" : "",
+
+      memoria: res.memoriaI ? "I" :
+        res.memoriaNPI ? "NPI" :
+          res.memoriaNP ? "NP" :
+            res.memoriaNPS ? "NPS" :
+              res.memoriaS ? "S" : "",
+
+      atencionConcentracion: res.atencionI ? "I" :
+        res.atencionNPI ? "NPI" :
+          res.atencionNP ? "NP" :
+            res.atencionNPS ? "NPS" :
+              res.atencionS ? "S" : "",
+
+      coordinacionVisoMotora: res.coordinacionI ? "I" :
+        res.coordinacionNPI ? "NPI" :
+          res.coordinacionNP ? "NP" :
+            res.coordinacionNPS ? "NPS" :
+              res.coordinacionS ? "S" : "",
+
+      orientacionEspacial: res.orientacionI ? "I" :
+        res.orientacionNPI ? "NPI" :
+          res.orientacionNP ? "NP" :
+            res.orientacionNPS ? "NPS" :
+              res.orientacionS ? "S" : "",
+
+      // Aspectos de Personalidad
+      estabilidad: res.estabilidadInestable ? "INESTABLE" :
+        res.estabilidadEstable ? "ESTABLE" : "",
+
+      ansiedadTendencias: res.nivelAnsiedadCaso ? "CASO" :
+        res.nivelAnsiedadNoCaso ? "NO CASO" : "",
+
+      consumoAlcohol: res.consumoAlcoholCaso ? "CASO" :
+        res.consumoAlcoholNoCaso ? "NO CASO" : "",
+
+      fobiaAltura: res.fobiaAlturaNada ? "NADA" :
+        res.fobiaAlturaLigeramente ? "LIGERAMENTE" :
+          res.fobiaAlturaModeradamente ? "MODERADAMENTE" :
+            res.fobiaAlturaMarcadamente ? "MARCADAMENTE" :
+              res.fobiaAlturaMiedoExtremo ? "MIEDO EXTREMO" : "",
+
+      // Analisis y recomendaciones
+      analisisResultados: res.analisis ?? "",
+      recomendaciones: res.recomendacion ?? "",
     }));
   }
 };
@@ -57,6 +125,60 @@ export const SubmitDataService = async (
 
   const body = {
     norden: form.norden,
+    fecha: form.fechaExamen,
+
+    nombreExamen: form.nombreExamen,
+
+    razonamientoI: form.razonamiento == "I",
+    razonamientoNPI: form.razonamiento == "NPI",
+    razonamientoNP: form.razonamiento == "NP",
+    razonamientoNPS: form.razonamiento == "NPS",
+    razonamientoS: form.razonamiento == "S",
+
+    memoriaI: form.memoria == "I",
+    memoriaNPI: form.memoria == "NPI",
+    memoriaNP: form.memoria == "NP",
+    memoriaNPS: form.memoria == "NPS",
+    memoriaS: form.memoria == "S",
+
+    atencionI: form.atencionConcentracion == "I",
+    atencionNPI: form.atencionConcentracion == "NPI",
+    atencionNP: form.atencionConcentracion == "NP",
+    atencionNPS: form.atencionConcentracion == "NPS",
+    atencionS: form.atencionConcentracion == "S",
+
+    coordinacionI: form.coordinacionVisoMotora == "I",
+    coordinacionNPI: form.coordinacionVisoMotora == "NPI",
+    coordinacionNP: form.coordinacionVisoMotora == "NP",
+    coordinacionNPS: form.coordinacionVisoMotora == "NPS",
+    coordinacionS: form.coordinacionVisoMotora == "S",
+
+    orientacionI: form.orientacionEspacial == "I",
+    orientacionNPI: form.orientacionEspacial == "NPI",
+    orientacionNP: form.orientacionEspacial == "NP",
+    orientacionNPS: form.orientacionEspacial == "NPS",
+    orientacionS: form.orientacionEspacial == "S",
+
+    estabilidadInestable: form.estabilidad == "INESTABLE",
+    estabilidadEstable: form.estabilidad == "ESTABLE",
+
+    nivelAnsiedadCaso: form.ansiedadTendencias == "CASO",
+    nivelAnsiedadNoCaso: form.ansiedadTendencias == "NO CASO",
+
+    consumoAlcoholCaso: form.consumoAlcohol == "CASO",
+    consumoAlcoholNoCaso: form.consumoAlcohol == "NO CASO",
+
+    fobiaAlturaNada: form.fobiaAltura == "NADA",
+    fobiaAlturaLigeramente: form.fobiaAltura == "LIGERAMENTE",
+    fobiaAlturaModeradamente: form.fobiaAltura == "MODERADAMENTE",
+    fobiaAlturaMarcadamente: form.fobiaAltura == "MARCADAMENTE",
+    fobiaAlturaMiedoExtremo: form.fobiaAltura == "MIEDO EXTREMO",
+
+    apto: form.esApto,
+    noApto: !form.esApto,
+
+    analisis: form.analisisResultados,
+    recomendacion: form.recomendaciones,
 
     usuarioRegistro: user,
   };
@@ -109,10 +231,13 @@ const GetInfoPac = async (nro, set, token, sede) => {
     set((prev) => ({
       ...prev,
       ...res,
+      nombres: res.nombresApellidos ?? "",
       fechaNacimiento: formatearFechaCorta(res.fechaNac ?? ""),
-      edad: (res.edad ? `${res.edad} AÑOS` : prev.edad),
-      ocupacion: res.areaO ?? prev.ocupacion,
-      cargoDesempenar: res.cargo ?? prev.cargoDesempenar,
+      edad: res.edad,
+      ocupacion: res.areaO ?? "",
+      cargoDesempenar: res.cargo ?? "",
+      lugarNacimiento: res.lugarNacimiento ?? "",
+      sexo: res.genero === "M" ? "MASCULINO" : "FEMENINO",
     }));
   }
 };
