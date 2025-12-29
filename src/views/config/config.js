449978a3f97f6de888eba2d8a6d956f5f9773fab
PVillanueva <pierofabrizzio9@hotmail.com>
2025-12-29 14:15:28 -0500

    Pv config

--- src/views/config/config.js
+++ src/views/config/config.js
@@ -1,7 +1,7 @@
 //Este archivo sirve para poder cambiar de rutas para las apis
 // main https://hmintegracion.azurewebsites.net
 // desarrollo https://testbackendhm.azurewebsites.net
-export const URLAzure = 'https://testbackendhm.azurewebsites.net'
+export const URLAzure = 'https://hmintegracion.azurewebsites.net'
 export const URLAPIREST = 'http://go.server.net.pe:3000/api/v2'
 export const TokenURL = 'horizontemedic_dtcm9'
 
