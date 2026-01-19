27434f4985611ffe34988b06f3e0f104eca95a17
Daniel Plasencia <daniel.plasencia.dlc@gmail.com>
2026-01-19 14:26:55 -0500

    cambio de coordenadas de firmas y sellos

--- src/views/admin/panel-de-control/SistemaOcupacional/Altura18/controllerAltura18.jsx
+++ src/views/admin/panel-de-control/SistemaOcupacional/Altura18/controllerAltura18.jsx
@@ -457,9 +457,9 @@ export const Loading = (mensaje) => {
 
 export const handleSubirArchivo = async (form, selectedSede, userlogued, token) => {
     const coordenadas = {
-        HUELLA: { x: 40, y: 680, width: 60, height: 60 },
-        FIRMA: { x: 106, y: 680, width: 120, height: 60 },
-        SELLOFIRMA: { x: 400, y: 680, width: 120, height: 80 },
+        HUELLA: { x: 400, y: 680, width: 60, height: 60 },
+        FIRMA: { x: 466, y: 680, width: 120, height: 60 },
+        SELLOFIRMA: { x: 40, y: 680, width: 120, height: 80 },
     };
     handleSubirArchivoDefault(form, selectedSede, registrarPDF, userlogued, token, coordenadas)
 };
