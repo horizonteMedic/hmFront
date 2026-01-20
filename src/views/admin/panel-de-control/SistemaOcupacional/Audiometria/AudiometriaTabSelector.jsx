ad810c23828b83f899132da46545c35ac191cdb8
Daniel Plasencia <daniel.plasencia.dlc@gmail.com>
2026-01-20 15:21:48 -0500

    error de Dan Audiometria

--- src/views/admin/panel-de-control/SistemaOcupacional/Audiometria/AudiometriaTabSelector.jsx
+++ src/views/admin/panel-de-control/SistemaOcupacional/Audiometria/AudiometriaTabSelector.jsx
@@ -10,71 +10,7 @@ const today = `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(
   "0"
 )}-${String(date.getDate()).padStart(2, "0")}`;
 
-const { userlogued, userName } = useSessionData();
 
-const initialFormStateCuestionarioAudio = {
-  norden: "",
-  codCuestionario: null,
-  fecha: today,
-  nombres: "",
-  edad: "",
-
-  genero: "",
-
-  // Preguntas del cuestionario
-  p1: "NO",
-  p2: "NO",
-  p3: "NO",
-  p4: "NO",
-  p5: "NO",
-  p6: "NO",
-  p7: "NO",
-  p8: "NO",
-  p9: "NO",
-  p10: "NO",
-  p11: "NO",
-  p12: "NO",
-  p13: "NO",
-  p14: "NO",
-  p15: "NO",
-  // Campos condicionales
-  p1_cual: "",
-  p1_cuando: "",
-  p1_quehizo: "",
-  p9_cual: "",
-  p9_donde: "",
-  p9_quehizo: "",
-  p10_cual: "",
-  p10_donde: "",
-  p10_quehizo: "",
-  p11_cual: "",
-  p11_tiempo: "",
-  p12_cual: "",
-  p12_tiempo: "",
-  p13_tiempo: "",
-  p13_cuando: "",
-  p13_donde: "",
-  p14_cual: "",
-  p14_donde: "",
-  p14_quehizo: "",
-  p15_cuantos: "",
-  // Pregunta 16 (todas en false por defecto)
-  p16_caza: false,
-  p16_caza_tiempo: "",
-  p16_tiro: false,
-  p16_tiro_tiempo: "",
-  p16_discoteca: false,
-  p16_discoteca_tiempo: "",
-  p16_auriculares: false,
-  p16_auriculares_tiempo: "",
-  p16_servicio: false,
-  p16_servicio_tiempo: "",
-  p16_boxeo: false,
-  p16_boxeo_tiempo: "",
-  // Médico que Certifica //BUSCADOR
-  nombre_medico: userName,
-  user_medicoFirma: userlogued,
-};
 
 export default function AudiometriaTabSelector({
   token,
@@ -83,6 +19,72 @@ export default function AudiometriaTabSelector({
   listas,
   subTab,
 }) {
+
+  const { userName } = useSessionData();
+  console.log(userlogued)
+  const initialFormStateCuestionarioAudio = {
+    norden: "",
+    codCuestionario: null,
+    fecha: today,
+    nombres: "",
+    edad: "",
+
+    genero: "",
+
+    // Preguntas del cuestionario
+    p1: "NO",
+    p2: "NO",
+    p3: "NO",
+    p4: "NO",
+    p5: "NO",
+    p6: "NO",
+    p7: "NO",
+    p8: "NO",
+    p9: "NO",
+    p10: "NO",
+    p11: "NO",
+    p12: "NO",
+    p13: "NO",
+    p14: "NO",
+    p15: "NO",
+    // Campos condicionales
+    p1_cual: "",
+    p1_cuando: "",
+    p1_quehizo: "",
+    p9_cual: "",
+    p9_donde: "",
+    p9_quehizo: "",
+    p10_cual: "",
+    p10_donde: "",
+    p10_quehizo: "",
+    p11_cual: "",
+    p11_tiempo: "",
+    p12_cual: "",
+    p12_tiempo: "",
+    p13_tiempo: "",
+    p13_cuando: "",
+    p13_donde: "",
+    p14_cual: "",
+    p14_donde: "",
+    p14_quehizo: "",
+    p15_cuantos: "",
+    // Pregunta 16 (todas en false por defecto)
+    p16_caza: false,
+    p16_caza_tiempo: "",
+    p16_tiro: false,
+    p16_tiro_tiempo: "",
+    p16_discoteca: false,
+    p16_discoteca_tiempo: "",
+    p16_auriculares: false,
+    p16_auriculares_tiempo: "",
+    p16_servicio: false,
+    p16_servicio_tiempo: "",
+    p16_boxeo: false,
+    p16_boxeo_tiempo: "",
+    // Médico que Certifica //BUSCADOR
+    nombre_medico: userName,
+    user_medicoFirma: userlogued.sub,
+  };
 
 
   const [formCuestionarioAudio, setFormCuestionarioAudio] = useState(
