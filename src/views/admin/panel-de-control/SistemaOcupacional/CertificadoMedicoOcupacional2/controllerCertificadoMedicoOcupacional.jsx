571e02e708ff14ca7b3c1d83cf30f06371556e16
Piero Villanueva <pierofabrizzio9@hotmail.com>
2026-01-28 12:17:07 -0500

    Pv cambios Geri

--- src/views/admin/panel-de-control/SistemaOcupacional/CertificadoMedicoOcupacional2/controllerCertificadoMedicoOcupacional.jsx
+++ src/views/admin/panel-de-control/SistemaOcupacional/CertificadoMedicoOcupacional2/controllerCertificadoMedicoOcupacional.jsx
@@ -1,45 +1,166 @@
-import Swal from "sweetalert2";
-import {
-    GetInfoPacDefault,
-    GetInfoServicioDefault,
-    LoadingDefault,
-    PrintHojaRDefault,
-    SubmitDataServiceDefault,
-    VerifyTRDefault,
-} from "../../../../../utils/functionUtils";
-import { getHoraActual } from "../../../../../utils/helpers";
 
 const obtenerReporteUrl = "/api/v01/ct/anexos/anexo16/obtenerReporteConsentimientoInformado";
 const registrarUrl = "/api/v01/ct/anexos/anexo16/registrarActualizarConsentimientoInformado";
 
-export const GetInfoServicio = async (nro, tabla, set, token, onFinish = () => {}) => {
-    const res = await GetInfoServicioDefault(nro, tabla, token, obtenerReporteUrl, onFinish);
-    if (res) {
-        set((prev) => ({
-            ...prev,
-            ...res,
-            norden: res.norden ?? prev.norden ?? "",
-            fecha: res.fecha ?? prev.fecha ?? "",
-            nombres: res.nombres ?? prev.nombres ?? "",
-            dni: res.dni ?? prev.dni ?? "",
 
-        }));
-    }
-};
 
-export const SubmitDataService = async (form, token, user, limpiar, tabla, datosFooter) => {
-    if (!form.norden) {
-        await Swal.fire("Error", "Datos Incompletos", "error");
-        return;
-    }
+export const Valores = {
+    CHECK1: `- LABORATORIO: Hb / Hto, Grupo Sanguíneo y Factor ({grupoFactor}), Glucosa, Ex. orina, 
+        - APTITUD
+- ANEXO 16
+- ANTECEDENTES ENFERMEDADES EN ALTURA
+- ANEXO 16-A
+- USO DE RESPIRADORES
+- HISTORIA OCUPACIONAL
+- ANTECEDENTES PATOLOGICOS
+- CUESTIONARIO NORDICO
+- MUSCULO ESQUELETICO
+- OIT
+- ESPIROMETRIA
+- AUDIOMETRIA
+- ODONTOLOGIA
+- PSICOLOGIA
+- AG. VISUAL
+- CONSENTIMIENTO INFORMADO
+- DECLARACION DE BUENA SALUD
+- COPIA DE DNI
 
-    const body = {
-        norden: form.norden,
-        fecha: form.fecha,
-        hora: getHoraActual(),
-        userRegistro: user,
-    };
-    await SubmitDataServiceDefault(token, limpiar, body, registrarUrl, () => {
-        PrintHojaR(form.norden, token, tabla, datosFooter);
-    });
+
+        `,
+
+    CHECK2: `- LABORATORIO: Hma, Hb / Hto, Grupo Sanguíneo y Factor ({grupoFactor}), Glucosa, Ex. orina, 
+        - APTITUD MÉDICA
+- ANEXO 16
+- ANTECEDENTES ENFERMEDADES EN ALTURA
+- ANEXO 16-A
+- PSICOSENSOMETRIA
+- CERTIFICADO DE CONDUCCION
+- FICHA SAS
+- USO DE RESPIRADORES
+- HISTORIA OCUPACIONAL 
+- ANTECEDENTES PATOLOGICOS
+- CUESTIONARIO NORDICO
+- FICHA MUSCULO ESQUELETICO
+- OIT
+- ESPIROMETRIA
+- AUDIOMETRIA
+- ODONTOLOGIA
+- INFORME PSICOLOGICO
+- AGUDEZA VISUAL
+- DECLARACION DE BUENA SALUD
+- CONSENTIMIENTO INFORMADO
+- COPIA DE DNI
+- COPIA DE LICENCIA DE CONDUCIR
+
+
+`,
+
+    CHECK3: `- LABORATORIO: Hma, Hb / Hto, Glucosa, Ex. Orina,Grupo Sanguineo y Factor ({grupoFactor})
+  - APTITUD MÉDICA
+- ANEXO 16
+- HISTORIA OCUPACIONAL 
+- ANTECEDENTES PATOLOGICOS 
+- CUESTIONARIO NORDICO
+- FICHA MUSCULO ESQUELETICO 
+- OIT 
+- ESPIROMETRIA
+- AUDIOMETRIA
+- ODONTOLOGIA
+- AGUDEZA VISUAL
+- CONSENTIMIENTO DE BUENA SALUD
+- CONSENTIMIENTO INFORMADO
+- COPIA DE DNI
+
+
+`,
+
+    CHECK4: `- LABORATORIO: Hma, Hb / Hto, Glucosa, Ex. Orina.
+  - APTITUD MÉDICA
+- ANEXO 16
+-  HISTORIA OCUPACIONAL 
+- ANTECEDENTES PATOLOGICOS 
+- CUESTIONARIO NORDICO
+- FICHA MUSCULO ESQUELETICO 
+- OIT
+-  ESPIROMETRIA
+- AUDIOMETRIA 
+- PSICOLOGIA
+- OFTALMOLOGIA 
+- CONSENTIMIENTO INFORMADO
+- COPIA DE DNI
+
+
+`,
+
+    CHECK5: `- LABORATORIO: Hma, Hb /Hto, Grupo Sanguineo y Factor ({grupoFactor})
+- Glucosa, Creatinina /dl, Ex. Orina, RPR, Test cualitativo: Cocaína y Marihuana, Colesterol y Triglicéridos
+- APTITUD MÉDICA
+- ANEXO 16
+- ANT. ENFERMEDADES ALTURA
+- ANEXO 16 – A
+- CERT. DE SUFICIENCIA MEDICA PARA TRABAJO EN ALTURA
+- PSICOSENSOMETRICO 
+- CERT. DE SUFICIENCIA MEDICA PARA CONDUCCION DE VEHICULO                           
+- FICHA S.A.S.
+- USO DE RESPIRADORES
+- HISTORIA OCUPACIONAL 
+- ANTECEDENTES PATOLOGICOS 
+- CUESTIONARIO NORDICO
+- FICHA MUSCULO ESQUELETICO 
+- OIT
+- ESPIROMETRIA   
+- EKG 
+- ODONTOLOGIA
+- AUDIOMETRIA 
+- OFTALMOLOGIA 
+- PSICOLOGIA
+- CONSENTIMIENTO INFORMADO
+- COPIA DE DNI
+- COPIA DE LICENCIA DE CONDUCIR
+
+`,
+
+    CHECK6: `- LABORATORIO:  Hb /Hto, Glucosa, Ex. Orina NORMAL, 
+  - APTITUD MÉDICA
+- ANEXO 16
+- HISTORIA OCUPACIONAL 
+- ANTECEDENTES PATOLOGICOS 
+- CUESTIONARIO NORDICO
+- FICHA MUSCULO ESQUELETICO 
+- OIT
+- AUDIOMETRIA 
+- OFTALMOLOGIA 
+- CONSENTIMIENTO INFORMADO
+- COPIA DE DNI
+
+
+`,
+
+    CHECK7: `- LABORATORIO: Hma, Hb /Hto, Grupo Sanguineo y Factor ({grupoFactor}), Glucosa, Creatinina /dl, Ex. Orina NORMAL, Vsg , RPR,
+  - TEST CUALITATIVO: COLESTEROL Y TRIGLICÉRIDOS
+- APTITUD MÉDICA
+- ANEXO 16
+- ANT. ENFERMEDADES ALTURA
+- ANEXO 16 – A
+- CERT. DE SUFICIENCIA MEDICA PARA TRABAJO EN ALTURA
+- PSICOSENSOMETRICO
+- CERT. DE CONDUCCION DE VEHICULOS
+- USO DE RESPIRADORES
+- HISTORIA OCUPACIONAL 
+- ANTECEDENTES PATOLOGICOS 
+- CUESTIONARIO NORDICO
+- FICHA MUSCULO ESQUELETICO 
+- OIT
+- ESPIROMETRIA
+- EKG
+- AUDIOMETRIA 
+- ODONTOLOGIA
+ - OFTALMOLOGIA 
+- PSICOLOGIA
+- CONSENTIMIENTO INFORMADO
+- COPIA DE DNI
+- COPIA DE LICENCIA
+
+
+`,
 };
