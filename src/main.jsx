483aa5eb5569cabd8cbe35d1d60e435f801fb92a
Jmmsimon <jeansimon176@gmail.com>
2026-02-19 09:14:57 -0500

    recuibr

--- src/main.jsx
+++ src/main.jsx
@@ -45,7 +45,7 @@ import Anexo16A from "./views/admin/panel-de-control/SistemaOcupacional/Anexo16A
 
 
 //SIMULADOR DE jaspers
-import Ficha from "./views/jaspers/MusculoEsqueletica/Digitalizado/EvaluacionMuscoloEsqueletica.jsx";
+import Ficha from "./views/jaspers/Oftalmologia/oftalmoJean/EvaluacionOftalmologica2021_Digitalizado.jsx";
 
 
 
