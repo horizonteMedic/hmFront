96ec36a6a4440688b9f0d5a6363a3337914b4544
Daniel Plasencia <daniel.plasencia.dlc@gmail.com>
2026-04-13 14:57:27 -0500

    despligue de anexo16

--- src/views/admin/panel-de-control/Valorizacion/model/controllerValo.jsx
+++ src/views/admin/panel-de-control/Valorizacion/model/controllerValo.jsx
@@ -1,9 +1,10 @@
+import Swal from "sweetalert2";
 import { SubmitData } from "../../../../utils/apiHelpers"
 import { LoadingDefault } from "../../../../utils/functionUtils"
 
 const urlSubmit = '/api/v01/st/registros/matrizControlInterno2026'
 
-export const SubmitValorizaciones = async (form, token,) => {
+export const SubmitValorizaciones = async (form, token, setData) => {
     const body = {
         "tipoPago": form.tipoPago,
         "contrata": form.razonContrata,
@@ -14,7 +15,15 @@ export const SubmitValorizaciones = async (form, token,) => {
     LoadingDefault('Realizando Busqueda')
     SubmitData(body, urlSubmit, token)
         .then((res) => {
-            console.log(res)
-
+            if (Array.isArray(res) && res.length > 0) {
+                console.log(res)
+                setData(res)
+                Swal.close()
+            } else {
+                Swal.fire("Error", "No se Encontrearon Registros", "error")
+            }
+        })
+        .catch(() => {
+            Swal.fire("Error", "Ocurrio un error en la consulta", "error")
         })
 }
