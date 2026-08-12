03b2a7b34ccb3942774adf8ac0276828bb360ae6
Piero Villanueva <pierofabrizzio9@hotmail.com>
2026-08-12 14:10:27 -0500

    Pv psicologia

--- src/views/admin/panel-de-control/SistemaOcupacional/ModuloPsicologia/ExamenesComplementarios2/InformeConductores/controllerInformeConductores.jsx
+++ src/views/admin/panel-de-control/SistemaOcupacional/ModuloPsicologia/ExamenesComplementarios2/InformeConductores/controllerInformeConductores.jsx
@@ -156,6 +156,7 @@ const GetInfoPac = async (nro, set, token, sede) => {
         set((prev) => ({
             ...prev,
             ...res,
+            fechaExamen: prev.fechaExamen,
             nombres: res.nombresApellidos ?? "",
             fechaNacimiento: formatearFechaCorta(res.fechaNac ?? ""),
             edad: res.edad,
